d4da4d6087fe5869e17796bda4e97a69946abfa2
Inkeliz <inkeliz@inkeliz.com>
2022-05-04 20:16:37 +0100

    add: zig package

---
+++ zig/karmem.zig
@@ -0,0 +1,91 @@
+const std = @import("std");
+const testing = std.testing;
+const Allocator = std.mem.Allocator;
+const mem = @import("std").mem;
+
+pub const Writer = struct {
+    memory: []u8,
+    capacity: usize,
+    allocator: Allocator,
+    isFixed: bool,
+
+    pub fn Alloc(self: *Writer, len: usize) Allocator.Error!u32 {
+        var offset: usize = self.memory.len;
+        var total: usize = offset + len;
+        if (total > self.capacity) {
+            var capacityTarget : usize = self.capacity * 2;
+            if (total > capacityTarget) {
+                capacityTarget = total;
+            }
+            try Writer.Grow(self, capacityTarget);
+        }
+        self.memory.len = total;
+        var i : usize = offset;
+        while(i < self.memory.len){
+            self.memory[i] = 0;
+            i = i + 1;
+        }
+        return @intCast(u32, offset);
+    }
+
+    pub fn WriteAt(self: *Writer, offset: u32, src: [*]const u8, len: usize) void {
+        @memcpy(self.memory.ptr[offset..offset+len].ptr, src, len);
+    }
+
+    pub fn Grow(self: *Writer, cap: usize) Allocator.Error!void {
+        if (self.isFixed) {
+            return std.mem.Allocator.Error.OutOfMemory;
+        }
+        const new_memory = try self.allocator.reallocAtLeast(self.memory.ptr[0..self.capacity], cap);
+        self.memory.ptr = new_memory.ptr;
+        self.capacity = new_memory.len;
+    }
+
+    pub fn Bytes(self: *Writer) []u8 {
+        return self.memory;
+    }
+
+    pub fn Reset(self: *Writer) void {
+        self.memory.len = 0;
+    }
+};
+
+pub fn NewWriter(allocator: Allocator, cap: usize) !Writer {
+    var r : Writer = .{
+       .memory = &[_]u8{},
+       .capacity = 0,
+       .allocator = allocator,
+       .isFixed = false,
+   };
+
+   try Writer.Grow(&r, cap);
+   return r;
+}
+
+pub fn NewFixedWriter(slice: []u8) Writer {
+    var r : Writer = .{
+       .memory = slice,
+       .capacity = slice.len,
+       .allocator = std.heap.page_allocator,
+       .isFixed = true,
+   };
+
+   Writer.Reset(&r);
+   return r;
+}
+
+pub const Reader = struct {
+    memory: []u8,
+    allocator: Allocator,
+
+    pub fn IsValidOffset(x: *Reader, offset: u32, size: u32) bool {
+        return @intCast(u64, x.memory.len) > (@intCast(u64, offset) + @intCast(u64, size));
+    }
+};
+
+pub fn NewReader(allocator: Allocator, memory: []u8) Reader {
+    return Reader {
+        .memory = memory,
+        .allocator = allocator,
+    };
+}

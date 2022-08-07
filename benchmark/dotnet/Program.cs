baa953b094c5f584097aa2c151e287181c7f8e11
Inkeliz <inkeliz@inkeliz.com>
2022-08-07 05:55:37 +0100

    benchmark: [dotnet] add exported functions with Mono trampoline
    That changes makes possible to compile to WASM with the standard
    benchmark exported functions.

--- benchmark/dotnet/Program.cs
+++ benchmark/dotnet/Program.cs
@@ -1,85 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using km;
 
-// Run test manually:
-unsafe
+// Init function
+var global = new dotnet.Benchmark(); // Init global benchmark object
+dotnet._Global.global = global; // Suppress GC
+dotnet.Benchmark.Ready(global); // Give the C code the pointer to the Benchmark class.
+
+namespace dotnet
 {
-    var g = new Global();
-    g.KBenchmarkDecodeObjectAPIFrom(g.InputMemory);
 
-    float expected = 50150224.000000F;
-    if (g.KBenchmarkDecodeSumVec3((uint)g.InputMemory.Length) != expected)
+    public static class _Global
     {
-        Console.WriteLine("KBenchmarkDecodeSumVec3 failed");
-        Console.WriteLine((uint)g.KBenchmarkDecodeSumVec3((uint)g.InputMemory.Length));
-    }
-    g.KBenchmarkDecodeObjectAPI((uint)g.InputMemory.Length);
-    g.KBenchmarkEncodeObjectAPI();
-}
-
-public class Global
-{
-    public byte[] InputMemory = File.ReadAllBytes("../testdata/kmbin/km.bin");
-    public IntPtr OutputMemory = Marshal.AllocHGlobal(8_000_000);
-    public km.Monsters Structure = new km.Monsters(); 
-    public Karmem.Reader Reader;
-    public Karmem.Writer Writer;
-
-    public Global()
-    {
-        Reader = Karmem.Reader.NewManagedReader(InputMemory);
-        Writer = Karmem.Writer.NewFixedWriter(OutputMemory, 8_000_000);
-    }
-    
-    // Must be exported to WASM.
-    public void KBenchmarkEncodeObjectAPI()
-    {
-        this.Writer.Reset();
-        if (!Structure.WriteAsRoot(this.Writer)) {
-            throw new System.Exception("Failed to write object");
-        }
+        public static dotnet.Benchmark? global = null;
     }
 
-    // Must be exported to WASM.
-    public void KBenchmarkDecodeObjectAPI(uint size)
+    public unsafe class Benchmark
     {
-        this.Reader.SetSize(size);
-        Structure.ReadAsRoot(this.Reader);
-    }
+        public IntPtr InputMemory = Marshal.AllocHGlobal(8_000_000);
+        public IntPtr OutputMemory = Marshal.AllocHGlobal(8_000_000);
 
-    // Must be exported to WASM.
-    public void KBenchmarkDecodeObjectAPIFrom(byte[] b)
-    {
-        var reader = Karmem.Reader.NewManagedReader(b);
-        Structure.ReadAsRoot(reader);
-        reader.Dispose();
-    }
+        public km.Monsters Structure = new km.Monsters();
+        public Karmem.Reader Reader;
+        public Karmem.Writer Writer;
 
-    // Must be exported to WASM.
-    public float KBenchmarkDecodeSumVec3(uint size)
-    {
-        this.Reader.SetSize(size);
-        
-        var monsters = MonstersViewer.NewMonstersViewer(this.Reader, 0);
-        var monstersList = monsters.Monsters(this.Reader);
-        
-        var sum = new Vec3();
-        for (var i = 0; i < monstersList.Count; i++)
+        public Benchmark()
         {
-            var path = monstersList[i].Data(this.Reader).Path(this.Reader);
-            for (var p = 0; p < path.Count; p++)
+            Reader = Karmem.Reader.NewReader(InputMemory, 8_000_000, 8_000_000);
+            Writer = Karmem.Writer.NewFixedWriter(OutputMemory, 8_000_000);
+        }
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void Ready(Benchmark owner);
+
+        public uint InputMemoryPointer()
+        {
+            return (uint)InputMemory.ToInt64();
+        }
+
+        public uint OutputMemoryPointer()
+        {
+            return (uint)OutputMemory.ToInt64();
+        }
+
+        // Must be exported to WASM.
+        public void KBenchmarkEncodeObjectAPI()
+        {
+            this.Writer.Reset();
+            if (!Structure.WriteAsRoot(this.Writer))
             {
-         
-                sum._X += path[p].X();
-                sum._Y += path[p].Y();
-                sum._Z += path[p].Z();
+                throw new System.Exception("Failed to write object");
             }
         }
 
-        return sum._X + sum._Y + sum._Z;
+        // Must be exported to WASM.
+        public void KBenchmarkDecodeObjectAPI(uint size)
+        {
+            this.Reader.SetSize(size);
+            Structure.ReadAsRoot(this.Reader);
+        }
+
+        // Must be exported to WASM.
+        public void KBenchmarkDecodeObjectAPIFrom(byte[] b)
+        {
+            var reader = Karmem.Reader.NewManagedReader(b);
+            Structure.ReadAsRoot(reader);
+            reader.Dispose();
+        }
+
+        // Must be exported to WASM.
+        public float KBenchmarkDecodeSumVec3(uint size)
+        {
+            this.Reader.SetSize(size);
+
+            var monsters = MonstersViewer.NewMonstersViewer(this.Reader, 0);
+            var monstersList = monsters.Monsters(this.Reader);
+
+            var sum = new Vec3();
+            for (var i = (ulong)0; i < monstersList.Count; i++)
+            {
+                var path = monstersList[i].Data(this.Reader).Path(this.Reader);
+                for (var p = (ulong)0; p < path.Count; p++)
+                {
+                    sum._X += path[p].X();
+                    sum._Y += path[p].Y();
+                    sum._Z += path[p].Z();
+                }
+            }
+            
+            return sum._X + sum._Y + sum._Z;
+        }
+
     }
-    
 }

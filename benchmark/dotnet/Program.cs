71b705b3b4670e42e1b0681fc4acf2851e89b291
Inkeliz <inkeliz@inkeliz.com>
2022-08-09 19:58:18 +0100

    benchmark: [dotnet] add support for TCP
    To compile for WASM you need to provide `-p:IS_WASM=true` building tags

--- benchmark/dotnet/Program.cs
+++ benchmark/dotnet/Program.cs
@@ -1,11 +1,79 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using km;
+using BitConverter = System.BitConverter;
 
-// Init function
+#if IS_WASM
+#else
+using System.Net;
+using System.Net.Sockets;
+#endif
+
 var global = new dotnet.Benchmark(); // Init global benchmark object
 dotnet._Global.global = global; // Suppress GC
-dotnet.Benchmark.Ready(global); // Give the C code the pointer to the Benchmark class.
+
+unsafe {
+#if IS_WASM // Compile with: dotnet build -c Release -p:IS_WASM=true -p:BYTEBUFFER_NO_BOUNDS_CHECK=true -p:UNSAFE_BYTEBUFFE=true
+
+    // Give the C code the pointer to the Benchmark class.
+    dotnet.Benchmark.Ready(global); 
+
+#else // Compile with: dotnet build -c Release -p:IS_WASM=false -p:BYTEBUFFER_NO_BOUNDS_CHECK=true -p:UNSAFE_BYTEBUFFE=true
+    try
+    {
+        var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 13000);
+        server.Start();
+
+        var buf = new Span<byte>(global.InputMemory.ToPointer(), 8_000_000);
+        var bufout = new ReadOnlySpan<byte>(global.OutputMemory.ToPointer(), 8_000_000);
+        var len = new byte[4];
+        var fn = new byte[4];
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            client.ReceiveTimeout = 3_600_000;
+            client.SendTimeout = 3_600_000;
+            
+            NetworkStream stream = client.GetStream();
+            
+            while (stream.Read(len, 0, 4) != 0)
+            {
+                stream.Read(fn, 0, 4);
+                if (BitConverter.ToInt32(len, 0) > 0)
+                {
+                    stream.ReadAtLeast(buf, BitConverter.ToInt32(len, 0));
+                }
+
+                switch (BitConverter.ToUInt32(fn, 0))
+                {
+                    case 1:
+                        Console.WriteLine(BitConverter.ToUInt32(len, 0));
+                        global.KBenchmarkDecodeObjectAPI(BitConverter.ToUInt32(len, 0));
+                        stream.Write(BitConverter.GetBytes(0));
+                        continue;
+                    case 2:
+                        global.KBenchmarkEncodeObjectAPI();
+                        stream.Write(BitConverter.GetBytes((uint)bufout.Length));
+                        stream.Write(bufout);
+                        continue;
+                    case 3:
+                        var r = global.KBenchmarkDecodeSumVec3(BitConverter.ToUInt32(len, 0));
+                        stream.Write(BitConverter.GetBytes(r));
+                        continue;
+                }
+            }
+        }
+    
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine(exception.ToString());
+    }
+    
+#endif
+
+}
 
 namespace dotnet
 {
